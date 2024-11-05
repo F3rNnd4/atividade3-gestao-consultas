1e7b100337b7aef857b01b129eb756e9aa62e7fc
Fernanda Louro <fernandaalves.tds2.senai@gmail.com>
2024-11-05 15:21:51 -0300

    feat: inserts into consultas

--- fernandaalves.sql
+++ fernandaalves.sql
@@ -1,20 +1,21 @@
 --CREATE DATABASE--
 
 CREATE DATABASE hospital;
+\c hospital;
 
 --CREATE TABLES--
 
 CREATE TABLE pacientes (
-    id_paciente SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL,
+id_paciente SERIAL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+cpf VARCHAR(11) UNIQUE NOT NULL
 );
 
 CREATE TABLE medicos (
-    id_medico SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    especialidade VARCHAR(100) NOT NULL,
-)
+id_medico SERIAL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+especialidade VARCHAR(100) NOT NULL
+);
 
 --INSERTS em pacientes--
 
@@ -31,16 +32,25 @@ INSERT INTO medicos (nome, especialidade) VALUES
 ('Dr. Fernando', 'Cardiologista'),
 ('Dra. Helena', 'Ginecologista'),
 ('Dra. Regina', 'Pediatra'),
-('Dra. Angélica', 'Dermatologista')
-('Drs. Wilma', 'Gastroenterologista')
+('Dra. Angélica', 'Dermatologista'),
+('Dra. Wilma', 'Gastroenterologista');
 
 --CREATE TABLE consultas--
 
 CREATE TABLE consultas (
-    id_consulta SERIAL PRIMARY KEY,
-    id_paciente INT NOT NULL,
-    id_medico INT NOT NULL,
-    data_consulta DATE NOT NULL,
-    CONSTRAINT fk_paciente FOREIGN KEY (id_paciente) REFERENCES pacientes(id_paciente),
-    CONSTRAINT fk_medico FOREIGN KEY (id_medico) REFERENCES medicos(id_medico)
+id_consulta SERIAL PRIMARY KEY,
+id_paciente INT NOT NULL,
+id_medico INT NOT NULL,
+data_consulta DATE NOT NULL,
+CONSTRAINT fk_paciente FOREIGN KEY (id_paciente) REFERENCES pacientes(id_paciente),
+CONSTRAINT fk_medico FOREIGN KEY (id_medico) REFERENCES medicos(id_medico)
 );
+
+--INSERTS em consultas--
+
+INSERT INTO consultas (id_paciente, id_medico, data_consulta) VALUES
+(5, 1, '2023-09-10'),
+(4, 5, '2024-10-15'),
+(3, 2, '2024-10-15'),
+(2, 3, '2024-10-15'),
+(1, 4, '2024-10-15');
