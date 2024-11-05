982b95295eada2d0aa72cb9566bf3ddbcd35244e
Fernanda Louro <fernandaalves.tds2.senai@gmail.com>
2024-11-05 14:48:48 -0300

    feat: inserts in pacientes and medicos

--- fernandaalves.sql
+++ fernandaalves.sql
@@ -15,3 +15,21 @@ CREATE TABLE medicos (
     nome VARCHAR(100) NOT NULL,
     especialidade VARCHAR(100) NOT NULL,
 )
+
+--INSERTS em pacientes--
+
+INSERT INTO pacientes (nome,cpf) VALUES
+('Nathalia Santos', '12345678901'),
+('Daniel Braga', '12345678902'),
+('Felipe Silva', '12345678903'),
+('Jéssica Barros', '12345678904'),
+('Chico Lourenço', '12345678905');
+
+--INSERTS em medicos--
+
+INSERT INTO medicos (nome,especialidade) VALUES
+('Dr. Fernando', 'Cardiologista'),
+('Dra. Helena', 'Ginecologista'),
+('Dra. Regina', 'Pediatra'),
+('Dra. Angélica', 'Dermatologista')
+('Drs. Wilma', 'Gastroenterologista')
